3f0c1a5fd68f1897fe773d2e9bec5005ba0da917
Andrew Williams <1williamsaj@gmail.com>
2019-09-21 19:52:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
 
-    BRANDS.each do |unique_brand|
+    BRANDS.any? do |unique_brand|
       if unique_brand == brand
         BRANDS << brand
       end
